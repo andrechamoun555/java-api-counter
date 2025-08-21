9daa13cbeeafa50ad7240380a1d2be6a5403d349
andrechamoun555 <andre.chamoun03@gmail.com>
2025-08-21 18:04:55 +0200

    first extension is done

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,13 +1,19 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
     private int value;
+    private String name;
+    private final Map<String, Integer>  counter = new HashMap<>();
 
     public CounterController() {
         this.value = 0;
@@ -19,10 +25,39 @@ public class CounterController {
     }
 
 
+
     @GetMapping("/increment")
     public int increment() {
         value++;
         return value;
+    }
+
+    @GetMapping("/custom/{name}/increment")
+    public String increment(@PathVariable("name") String name) {
+        if (counter.containsKey(name)) {
+            int current = counter.get(name);
+            int newValue = current + 1;
+            counter.put(name, newValue);
+            return "hello " + name + " this is you new value: " + newValue;
+        } else {
+            counter.put(name, 1);
+            return "hello " + name + " this is you new value: " + 1;
+        }
+
+    }
+
+    @GetMapping("/custom/{name}/decrement")
+    public String decrement(@PathVariable("name") String name) {
+        if (counter.containsKey(name)) {
+            int current = counter.get(name);
+            int newValue = current - 1;
+            counter.put(name, newValue);
+            return "Hello " + name + ", you new points are: " + newValue;
+        } else {
+            counter.put(name, -1);
+            return "Hello " + name + " this is you new points are: " + -1;
+        }
+
     }
 
     @GetMapping("/decrement")
@@ -31,16 +66,15 @@ public class CounterController {
         return value;
     }
 
+    @GetMapping("/custom/{name}")
+    public String custom(@PathVariable String name) {
+        if (counter.containsKey(name)) {
+            return "Hello " + name + ", this is your current value:" + counter.get(name);
+        } else {
+            counter.put(name, 0);
+            return "Hello " + name + ", you are new. this is your current value:" + counter.get(name);
+        }
+    }
+
+
 }
-
-//Annotate the CounterController class appropriately to set it up as a REST controller,
-//and map all requests of http://localhost:4000/counter to it
-
-
-//Create a GET method that responds with the current value of the counter. Its initial value should always be 0
-
-//Create a GET method that maps to the route http://localhost:4000/counter/increment that increases the counter by 1 and responds
-// with the updated value. The method you created in step 2 should also now respond with the updated value
-
-//Create a GET method that maps to the route http://localhost:4000/counter/decrement that decreases the counter by 1
-// and responds with the updated value
